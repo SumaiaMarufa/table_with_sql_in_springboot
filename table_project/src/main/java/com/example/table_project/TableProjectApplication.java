cca41fd9ab5e0f0845b291aa1fb88ddae3b785e9
Sumaia Marufa <160595513+SumaiaMarufa@users.noreply.github.com>
2025-01-01 17:05:13 +0600

    Add files via upload

---
+++ table_project/src/main/java/com/example/table_project/TableProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.table_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableProjectApplication.class, args);
+	}
+
+}
